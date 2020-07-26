b30cd29c1cd83ea0d92740f6f0de8f571cd262ca
Javier Chávarri <javier.chavarri@gmail.com>
2020-07-26 11:26:06 +0200

    test cache

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,5 @@
 open Opium.Std
 
-let hello = get "/" (fun _ -> `String "Hello world" |> respond')
+let hello = get "/" (fun _ -> `String "Hello World" |> respond')
 
 let () = App.empty |> hello |> App.run_command |> ignore
